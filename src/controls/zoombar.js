797397471ddebbea8f0eddfeae60d695a288e9ae
sorskoot <mental_c@hotmail.com>
2016-11-18 12:33:43 +0100

    Added dragging to the zoombar

--- src/controls/zoombar.js
+++ src/controls/zoombar.js
@@ -32,17 +32,52 @@
                 },
                 setupEvents: function () {
                     this.zoombarOverlayCanvas.onmousemove = (e) => {
-                        let currentPos = {x:e.offsetX,y:e.offsetY};
+                        let currentPos = { x: e.offsetX, y: e.offsetY };
                         let startAndEnd =
                             calculateStartAndEnd(this.zoombarOverlayCanvas.width, this.startPosition, this.endPosition);
 
                         if (currentPos.x > startAndEnd.start &&
-                            currentPos.x < startAndEnd.end)
+                            currentPos.x < startAndEnd.end) {
+
                             this.zoombarOverlayCanvas.style.cursor = 'pointer';
-                        else{
+
+                            if (this.dragging) {
+                                var mousePos = (1.0 / this.zoombarOverlayCanvas.width) * (this.draggingStartPos - currentPos.x);
+                                if (this.oldStartPosition - mousePos > 0 && this.oldEndPosition - mousePos < 1) {
+
+                                    if (this.oldStartPosition - mousePos < 0.05) {
+                                        mousePos = this.oldStartPosition;
+                                    }
+                                    if (this.oldEndPosition - mousePos > 0.95) {
+                                        mousePos = -1 + this.oldEndPosition;
+                                    }
+                                    this.startPosition = this.oldStartPosition - mousePos;
+                                    this.endPosition = this.oldEndPosition - mousePos;
+
+
+
+                                    this.redrawOverlay();
+                                }
+                            }
+                        }
+                        else {
                             this.zoombarOverlayCanvas.style.cursor = 'default';
                         }
                     };
+
+                    this.zoombarOverlayCanvas.onmousedown = e => {
+                        this.dragging = true;
+                        this.draggingStartPos = e.offsetX;
+                        this.oldStartPosition = this.startPosition;
+                        this.oldEndPosition = this.endPosition;
+                    }
+
+                    this.zoombarOverlayCanvas.onmouseup = e => {
+                        this.dragging = false;
+                    }
+                    this.zoombarOverlayCanvas.onmouseout = e => {
+                        this.dragging = false;
+                    }
                 },
                 drawBackground: function () {
                     // draw the tracks on the zoombar
@@ -59,18 +94,18 @@
                         calculateStartAndEnd(this.zoombarOverlayCanvas.width, this.startPosition, this.endPosition);
 
                     overlayCtx.clearRect(0, 0, this.zoombarOverlayCanvas.width, this.zoombarOverlayCanvas.height);
-                    overlayCtx.fillStyle = "rgba(255, 255, 255, 0.1)";
+                    overlayCtx.fillStyle = "rgba(0, 255, 0, 0.5)";
                     overlayCtx.fillRect(startAndEnd.start, 0, startAndEnd.end - startAndEnd.start, this.zoombarOverlayCanvas.height);
-                    overlayCtx.strokeStyle = "rgba(255, 255, 255, 0.7)";
+                    overlayCtx.strokeStyle = "rgba(0, 255, 0, 1)";
                     overlayCtx.strokeRect(startAndEnd.start, 0, startAndEnd.end - startAndEnd.start, this.zoombarOverlayCanvas.height);
                 }
 
             })
     });
 
-    function calculateStartAndEnd(canvasWidth, startPos, endPostion) {
+    function calculateStartAndEnd(canvasWidth, startPos, endPosition) {
         let start = canvasWidth * startPos;
-        let end = canvasWidth * endPostion;
+        let end = canvasWidth * endPosition;
 
         return { start: start, end: end };
     }
